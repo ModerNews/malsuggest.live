66233c568b43eae74331efd0cf667d36ba3045d1
Gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2022-11-13 11:40:21 +0100

    Adding dynamic redirect value (adapting to running prod and development simultaneously

--- frontend/app.py
+++ frontend/app.py
@@ -14,6 +14,7 @@ import malclient
 class App(Flask):
     def __init__(self):
         super().__init__(__name__, static_folder='./static', template_folder='./templates')
+        self.debug_value = False
 
 
 app = App()
@@ -23,9 +24,9 @@ app = App()
 def index():
     url, code_verifier = malclient.generate_authorization_url('48563b906310d3fdb4cefa1c1877bfc3')
     logging.info(code_verifier)
-    response = make_response(render_template('index.html', redirect_url=url), 200)
-    response.set_cookie('code_verifier', value=base64.b64encode(code_verifier.encode()).decode(), expires=(
-                datetime.datetime.now(tz=zoneinfo.ZoneInfo('Europe/London')) + datetime.timedelta(minutes=3)))
+    response = make_response(render_template('index.html', redirect_url=url + '&redirect_uri=' + os.getenv('MAL_REDIRECT', 'http://127.0.0.1:5000/redirect')), 200)
+    response.set_cookie('code_verifier', value=base64.b64encode(code_verifier.encode()).decode(),
+                        expires=(datetime.datetime.now(tz=zoneinfo.ZoneInfo('Europe/London')) + datetime.timedelta(minutes=3)))
     return response
 
 
@@ -42,7 +43,8 @@ def redirect_receive():
     token_response = malclient.fetch_token_schema_2(client_id=os.getenv('MAL_CLIENT_ID'),
                                                     client_secret=os.getenv('MAL_CLIENT_SECRET'),
                                                     code=request.args.get('code'),
-                                                    code_verifier=code_verifier)
+                                                    code_verifier=code_verifier,
+                                                    redirect_uri=os.getenv('MAL_REDIRECT', 'http://127.0.0.1:5000/redirect'))
     response = make_response(redirect(url_for('recommendations_page')), 302)
     response.set_cookie('access_token', value=token_response.get('access_token'), samesite='Strict', expires=(datetime.datetime.now() + datetime.timedelta(seconds=int(token_response.get('expires_in')))))
     response.set_cookie('refresh_token', value=token_response.get('refresh_token'), samesite='Strict', expires=(datetime.datetime.now() + datetime.timedelta(seconds=int(token_response.get('expires_in')))))
@@ -52,8 +54,11 @@ def redirect_receive():
 
 @app.get('/recommendations')
 def recommendations_page():
-    return render_template('loading.html')
-
+    if not app.debug_value:
+        app.debug_value = True
+        return render_template('loading.html')
+    else:
+        return render_template('results.html')
 
 @app.errorhandler(410)
 def code_verification_timeout(e):
