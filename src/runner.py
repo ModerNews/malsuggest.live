1fa4f9b0115cbb9ef924d3e236f9ea1e1cce274a
gruzin <grzegorz.jagielski.2019@zsa.pwr.edu.pl>
2023-03-03 11:09:44 +0100

    Fix to runner.py (getting rid of socket.io)

--- src/runner.py
+++ src/runner.py
@@ -1,8 +1,8 @@
-from app import create_app, socketio
+from app import create_app
 
 from app import celery
 
 
 if __name__ == "__main__":
     app = create_app(celery, app_name=__name__)
-    socketio.run(app, "0.0.0.0", allow_unsafe_werkzeug=True)
+    app.run("0.0.0.0", allow_unsafe_werkzeug=True)
